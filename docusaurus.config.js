fa07c4b98655c3437233c6564d3821e151595f18
wxvirus <1418667580@qq.com>
2022-10-12 23:52:02 +0800

    first commit

---
+++ docusaurus.config.js
@@ -0,0 +1,139 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: '无解的游戏',
+    tagline: '记录生活和学习',
+    url: 'https://www.wjstar.top',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'sword-demon', // Usually your GitHub org/user name.
+    projectName: 'sword-wiki', // Usually your repo name.
+    deploymentBranch: 'gh-pages',
+
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'zh-Hans',
+        locales: ['zh-Hans'],
+    },
+
+    presets: [
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    showLastUpdateAuthor: true,
+                    showLastUpdateTime: true,
+                    breadcrumbs: false,
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                    blogDescription: "wxvirus的个人生活和工作记录",
+                    blogSidebarCount: 7,
+                    blogSidebarTitle: "文章",
+                    feedOptions: {
+                        title: "wxvirus`s Blog",
+                        description: "wxvirus 的个人生活和工作记录",
+                        type: 'all',
+                        copyright: `Copyright © ${new Date().getFullYear()} wxvirus, Inc.`,
+                    },
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
+    ],
+
+    plugins: [
+        [
+            "@docusaurus/plugin-content-docs",
+            {
+                id: "dev",
+                path: "wiki/dev",
+                routeBasePath: "dev",
+                sidebarPath: require.resolve("./sidebars.js"),
+//                editUrl: "https://git.7wate.com/zhouzhongping/wiki/src/branch/master",
+                showLastUpdateAuthor: true,
+                showLastUpdateTime: true,
+                breadcrumbs: false,
+            },
+        ]
+    ],
+
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            metadata: [
+                {
+                    name: "keywords",
+                    content: "wxvirus, wiki, blog, php, c, go, k8s, linux",
+                },
+            ],
+            navbar: {
+                title: 'wxvirus',
+                hideOnScroll: true,
+                logo: {
+                    alt: 'My Site Logo',
+                    src: 'img/logo.svg',
+                },
+                items: [
+                    {
+                        position: "right",
+                        label: "笔记文档",
+                        items: [
+                            {
+                                label: "程序语言",
+                                to: "/dev",
+                            },
+                        ],
+                    },
+                    {
+                        type: 'doc',
+                        docId: 'intro',
+                        position: 'left',
+                        label: 'Tutorial',
+                    },
+                    {to: '/blog', label: '博客', position: 'left'},
+                    {
+                        href: 'https://github.com/sword-demon/sword-wiki',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                copyright: `Copyright © ${new Date().getFullYear()} 无解的游戏, Inc. Built with Docusaurus.`,
+            },
+            prism: {
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+                defaultLanguage: "markdown",
+                additionalLanguages: ["java", "git","nginx","http"],
+            },
+        }),
+};
+
+module.exports = config;
---
+++ wiki/dev/home.md
@@ -0,0 +1,8 @@
+---
+slug: /
+sidebar_position: 1
+---
+
+# Home
+
+开发中和学习中的笔记文档
